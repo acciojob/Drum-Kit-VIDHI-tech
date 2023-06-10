048b05805ada7159cd67b9135a877564dacb5b56
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-10 13:18:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-//your JS code here. If required.
 // Function to play the sound
 function playSound(e) {
-  // Find the audio element associated with the pressed key
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-  // Find the key element associated with the pressed key
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return; 
 
-  if (!audio) return;
   audio.currentTime = 0; 
-  audio.play(); 
+  audio.play();
 
-  // Add a class to the key element for visual effect
   key.classList.add('playing');
 }
 
-// Function to remove the visual effect class
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
   this.classList.remove('playing');
@@ -24,6 +19,6 @@ function removeTransition(e) {
 
 window.addEventListener('keydown', playSound);
 
-// Get all the keys and attach transitionend event listener to each
+
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
